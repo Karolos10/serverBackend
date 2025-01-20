c589ce6f2e9c5f224d1daa547a71c8de9f602ed0
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-19 22:20:08 -0500

    Added ping method to ServerService interface to ping a server by its IP address

--- src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
@@ -1,20 +1,45 @@
 package com.example.server.service.implementation;
 
 import com.example.server.model.Server;
+import com.example.server.repository.ServerRepository;
 import com.example.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
 import java.util.List;
 
+import static com.example.server.enumeration.Status.*;
+
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerService {
-    @Override
-    public Server create(Server server) {
-        return null;
+
+    private final ServerRepository serverRepository;
+
+    public ServerServiceImpl(ServerRepository serverRepository) {
+        this.serverRepository = serverRepository;
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server create(Server server) {
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImage());
+        return serverRepository.save(server);
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
     }
 
     @Override
@@ -34,6 +59,10 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImage() {
         return null;
     }
 }
