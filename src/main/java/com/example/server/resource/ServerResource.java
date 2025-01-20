986c2b7cdcc01ac260555d304ac86004e09ee0cc
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-20 13:30:57 -0500

    Add endpoints to ping and save servers in ServerResource

--- src/main/java/com/example/server/resource/ServerResource.java
+++ src/main/java/com/example/server/resource/ServerResource.java
@@ -1,15 +1,20 @@
 package com.example.server.resource;
 
+import com.example.server.enumeration.Status;
 import com.example.server.model.Response;
+import com.example.server.model.Server;
 import com.example.server.service.implementation.ServerServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Map;
 
+import static com.example.server.enumeration.Status.*;
 import static java.time.LocalDateTime.now;
+import static java.util.Map.*;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -22,14 +27,41 @@ public class ServerResource {
     }
 
     @GetMapping("/list")
-    public ResponseEntity<Response> getServers(){
+    public ResponseEntity<Response> getServers() {
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(now())
-                        .data(Map.of("servers", serverService.list(30)))
+                        .data(of("servers", serverService.list(30)))
                         .message("Servers retrieved successfully")
                         .status(OK)
                         .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("servers", server))
+                        .message(server.getStatus() == SERVER_UP ? "Server is up" : "Server is down")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@PathVariable @Valid Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("servers", serverService.create(server)))
+                        .message("Server created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
                         .build()
         );
     }
